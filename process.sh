ef3ad1a06e4da9ce2c44009e6687f99e8dcc4d28
Mikko Koho <mikko.koho@aalto.fi>
2019-08-06 15:51:48 +0300

    Fix hisco_45 missing zeros

--- process.sh
+++ process.sh
@@ -6,6 +6,9 @@ mkdir -p output/logs
 # Remove extra whitespace
 cat data/ammo.csv | sed -r "s/^\ +//g" | sed -r "s/\ *,\ */,/g" > output/ammo.csv
 
+# Change hisco_45 value 0 to 00
+cat data/hisco_45.csv | sed -r "s/^([0-9]+),0,/\1,00,/g" > output/hisco_45.csv
+
 # RML mapping
 $RML -m mapping.ttl -o output/_ammo.ttl -d
 $RML -m mapping_hisco.ttl -o output/_hisco.ttl -d -v
