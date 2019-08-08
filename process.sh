e598ebeb6a0e2719ffb506613ad34f95edadfae5
Mikko Koho <mikko.koho@aalto.fi>
2019-08-08 14:41:06 +0300

    Add additional HISCO resources

--- process.sh
+++ process.sh
@@ -18,7 +18,7 @@ python src/postprocess.py add_altlabels data/coo1980.ttl output/coo1980.ttl --lo
 
 python src/postprocess.py get_localnames output/_ammo.ttl output/_ammo_2.ttl --logfile output/logs/postprocess.log
 python src/postprocess.py remove_empty_literals output/_ammo_2.ttl output/_ammo_3.ttl --logfile output/logs/postprocess.log
-cat output/_ammo_3.ttl output/coo1980.ttl output/_hisco.ttl | rapper - "http://ldf.fi/ammo/" -i turtle -o turtle > output/_ammo_combined.ttl
+cat output/_ammo_3.ttl output/coo1980.ttl output/_hisco.ttl data/hisco_additional.ttl | rapper - "http://ldf.fi/ammo/" -i turtle -o turtle > output/_ammo_combined.ttl
 
 # python src/postprocess.py add_en_labels output/_ammo_combined.ttl output/_ammo_combined_2.ttl --logfile output/logs/postprocess.log
 python src/postprocess.py remove_unused_hisco output/_ammo_combined.ttl output/_ammo_.ttl --logfile output/logs/postprocess.log
