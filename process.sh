a4e4e65c60fffe2579daf6a43639fd8f769501ed
Mikko Koho <mikko.koho@aalto.fi>
2019-09-06 14:33:03 +0300

    Get COO1980 broader relations with skosify

--- process.sh
+++ process.sh
@@ -17,10 +17,11 @@ docker run --rm -v $(pwd):/data rmlmapper -m mapping_hisco.ttl -o output/_hisco.
 python src/postprocess.py add_altlabels data/coo1980.ttl output/_coo1980.ttl --logfile output/logs/postprocess.log
 python src/postprocess.py add_coo1980_members output/_coo1980.ttl output/_coo1980.ttl --logfile output/logs/postprocess.log
 
+skosify output/_coo1980.ttl -o output/_coo1980_skosified.ttl
+
 python src/postprocess.py get_localnames output/_ammo.ttl output/_ammo_2.ttl --logfile output/logs/postprocess.log
 python src/postprocess.py remove_empty_literals output/_ammo_2.ttl output/_ammo_3.ttl --logfile output/logs/postprocess.log
-cat output/_ammo_3.ttl output/_coo1980.ttl output/_hisco.ttl data/hisco_additional.ttl | rapper - "http://ldf.fi/ammo/" -i turtle -o turtle > output/_ammo_combined.ttl
-cat output/_ammo_3.ttl output/_coo1980.ttl output/_hisco.ttl data/hisco_additional.ttl input/collections_and_schemes.ttl input/ammo_additions.ttl | rapper - "http://ldf.fi/ammo/" -i turtle -o turtle > output/_ammo_combined.ttl
+cat output/_ammo_3.ttl output/_coo1980_skosified.ttl output/_hisco.ttl data/hisco_additional.ttl input/collections_and_schemes.ttl input/ammo_additions.ttl | rapper - "http://ldf.fi/ammo/" -i turtle -o turtle > output/_ammo_combined.ttl
 
 # python src/postprocess.py add_en_labels output/_ammo_combined.ttl output/_ammo_combined_2.ttl --logfile output/logs/postprocess.log
 python src/postprocess.py remove_unused_hisco output/_ammo_combined.ttl output/_ammo_.ttl --logfile output/logs/postprocess.log
